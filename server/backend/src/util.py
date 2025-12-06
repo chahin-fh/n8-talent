04a7265665541de060b9d4116bc5ad496ae0989f
chahin-fh <chahinfhima@gmail.com>
2025-12-06 12:19:25 +0100

    final

--- server/backend/src/util.py
+++ server/backend/src/util.py
@@ -5,7 +5,7 @@ def set_config(app):
 	app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
 	app.config["JWT_ACCESS_COOKIE_NAME"] = "ctoken"
 	app.config["JWT_COOKIE_CSRF_PROTECT"] = False
-	app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://carbon:carbon123@localhost:3306/flask_db"
+	app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost:3306/flask_db"
 	app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 def init_context(app, db):
