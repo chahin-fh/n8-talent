212fe59d3f2619b5659b526f4346cc3ee2cc0cf1
chahin-fh <119758474+chahin-fh@users.noreply.github.com>
2025-12-07 09:22:02 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -273,4 +273,5 @@ def verify():
 	return jsonify(logged_in_as=current_user), 200
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	import os
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
